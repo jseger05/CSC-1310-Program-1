d40fa73d41ba6eaf10860a92bbb017473e4b7803
jseger05 <jseger42@tntech.edu>
2024-09-13 22:22:01 -0500

    Implemented Media parent class

--- Media.cpp
+++ Media.cpp
@@ -1,1 +1,27 @@
+#include "Media.h"
+
 //This is the parent class -- should have stuff that all media share (title, publication date, etc) -- see Readme
+Media::Media(std::string title = "Default title", int year = 2000, double size = 0.0){
+    title = title;
+    year = year;
+    size = size;
+}
+
+std::string Media::getTitle(){
+    return title;
+}
+int Media::getYear(){
+    return year;
+}
+double Media::getSize(){
+    return size;
+}
+void Media::setTitle(std::string t){
+    title = t;
+}
+void Media::setYear(int y){
+    year = y;
+}
+void Media::setSize(double s){
+    size = s;
+}

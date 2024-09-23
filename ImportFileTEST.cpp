1ae37c44cd5553c82155c718011e37d242a4b5af
Jonathan <jdsalvato42@tntech.edu>
2024-09-23 18:20:15 -0500

    Modified Test Function for folder support

--- ImportFileTEST.cpp
+++ ImportFileTEST.cpp
@@ -3,7 +3,7 @@
 int main(){
     Shelf movie;
 
-    std::cout << importFile("movies.txt", "Movies", movie) << "\n\n";
+    std::cout << importFile("TEST_CASE_SRC/movies.txt", "Movies", movie) << "\n\n";
     
     for(int i = 0; i < movie.getSize(); i++){
         std::cout << movie.getItem(i).getTitle() << ", " << movie.getItem(i).getYear() << ", " <<movie.getItem(i).getSize() << "\n"; 

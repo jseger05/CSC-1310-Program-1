bb631cbb9e0a0e0d941bee3c4a14f98ce6a21daf
Jonathan <jdsalvato42@tntech.edu>
2024-09-23 17:17:25 -0500

    Defined Shelf Class

---
+++ Shelf.h
@@ -0,0 +1,45 @@
+/* 
+ * Written by Jonathan Salvato
+ * 9/23/24
+ * Purpose Dynamically allocate media types
+ */
+
+#include "Media.h"
+#include "Movie.h"
+#include "Music.h"
+#include "Tvshow.h"
+#include "Videogame.h"
+#include <vector>
+
+struct Shelf{
+    private:
+        std::vector<Media> media;
+        std::string type;
+        int size;
+    public:
+        Shelf(){
+            size = 0;
+            type = "Basic Media";
+        }
+        Shelf(std::string type){
+            this->type = type;
+        }
+        ~Shelf(){
+          media.clear();
+        }
+        Shelf(Media arr[]);
+        Shelf(Movie arr[]);
+        //until datatypes added, commented out
+        /*Shelf(Music arr[]);
+        Shelf(TvShow arr[]);
+        Shelf(Videogame arr[]);*/
+        void setItem(Media, int);
+        void setItem(std::string, int, double, int);
+        void addItem(Media);
+        void setType(std::string);
+        Media getItem(int);
+        std::string getType();
+        int getSize();
+
+    
+};

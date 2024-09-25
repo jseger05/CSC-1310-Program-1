aab098ac30523a81e3cfcc28486a8ccbf76a5c71
Jonathan <jdsalvato42@tntech.edu>
2024-09-25 10:56:40 -0500

    Implemented exportFile Function

--- ExportFile.cpp
+++ ExportFile.cpp
@@ -2,6 +2,20 @@
 #include <fstream>
 
 //returns true on successful write
-bool exportFile(std::string directory, const Shelf obj){
-    
+bool exportFile(std::string directory, Shelf obj){
+    std::ofstream outFile;
+    outFile.open(directory, std::ios::trunc);
+    for(int i = 0; i < obj.getSize(); i++){
+        for(int j = 0; j < 3; j++){
+            switch(j){
+                case(0): outFile << obj.getItem(i).getTitle(); break;
+                case(1): outFile << obj.getItem(i).getYear(); break;
+                case(3): outFile << obj.getItem(i).getSize(); break;
+            }
+            if(i != obj.getSize() - 1 || j != 3){
+                outFile << ",";
+            }
+        }
+        outFile << "\n";
+    }
 }

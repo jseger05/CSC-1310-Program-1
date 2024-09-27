f97890f02fc8238442cfca41acab4c9aca03f6b1
jseger05 <jseger42@tntech.edu>
2024-09-27 14:58:06 -0500

    Partial work on Driver

--- driver.cpp
+++ driver.cpp
@@ -1,5 +1,27 @@
-//import files
-//export files
-//write files
-//CLI to edit shelf -- menu, etc
-//CIN for test cases
+/* 
+ * Written by J Seger
+ * 9/27/24
+ * Driver file
+ */
+
+#include "Shelf.h"
+
+//Should:
+//At launch, import a test case via cin. This will be in delimited form of data for a shelf, plus maybe commands to make changes? Ask
+//Start CLI menu with options:
+//  Import file (adds to existing shelf)
+//  Export file (existing data to file -- automatic or no?)
+//  View contents of a shelf
+//      Options to do shelf options (delete element, etc) or edit element (Media obj get/set)
+
+int main(){
+    //main Storage class -- holds ptrs to shelves, which have ptrs to Media objects
+
+    //read test case
+
+    //create menu
+    do{
+        std::cout << "Welcome to Media Library!";
+    
+    }
+}

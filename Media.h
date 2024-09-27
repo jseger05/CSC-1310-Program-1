756da6ca55adb22c8febfce9de787c00bc762b24
NixBoss-arch <76533731+NixBoss-arch@users.noreply.github.com>
2024-09-27 14:04:35 -0500

    Reformat media.h for better readability
    No code was changed, only the formatting to space out sections in an attempt to make the code easier to read.

--- Media.h
+++ Media.h
@@ -3,26 +3,30 @@
 
 #include <iostream>
 
-class Media{
-    //parent class for other media
+class Media
+{
+    // parent class for other media
 
-    private:
-        std::string title;
-        std::string creator;
-        int year;
-        double num;
+private:
+    std::string title;
+    std::string creator;
+    int year;
+    double num;
 
-    public:
-        Media(std::string = "Default title", std::string = "Default Artist", int = 2000, double = 0.0);
+public:
+    Media(std::string = "Default title", std::string = "Default Artist", int = 2000, double = 0.0);
 
-        std::string getTitle();
-        std::string getCreator();
-        int getYear();
-        double getNum();
-        void setTitle(std::string);
-        void setCreator(std::string);
-        void setYear(int);
-        void setSize(double);
+    // Accessor Function Prototypes
+    std::string getTitle();
+    std::string getCreator();
+    int getYear();
+    double getNum();
+
+    // Mutator Function Prototypes
+    void setTitle(std::string);
+    void setCreator(std::string);
+    void setYear(int);
+    void setSize(double);
 };
 
 #endif
